ad9c38bf9a1133e4ecb4a0b89d791e995de2ff31
John Lemak <lemak91@mac.com>
2021-12-05 21:38:23 -0600

    Completed assignment

---
+++ index.js
@@ -0,0 +1,55 @@
+function add(number) {
+    return number + number;
+}
+
+function subtract(number) {
+    return number - number;
+}
+
+function multiply(number) {
+    return number * number;
+}
+
+function divide(number) {
+    return number / number;
+}
+
+function add(a,b) {
+    return a+b;
+}
+
+function subtract(a,b) {
+    return a-b;
+}
+
+function multiply(a,b) {
+    return a*b;
+}
+
+function divide(a,b) {
+    return a/b;
+}
+
+function increment(n) {
+    return n + 1;
+}
+
+function decrement(n) {
+    return n - 1
+}
+
+function makeInt(n) {
+    return parseInt(n);
+}
+
+function makeInt(n) {
+    return parseInt(n, 10);
+}
+
+function preserveDecimal(n) {
+    return parseInt(n);
+}
+
+function preserveDecimal(n) {
+    return parseFloat(n);
+}
